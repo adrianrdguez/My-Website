c36468751081f0884b5f8dfd40e7e57f4542e59d
adrianrdguez <adrian.rodriguez@singularfactory.com>
2021-07-23 13:24:43 +0100

    some changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var  language = "hola";
-/*--Enseñar Menu---*/ 
+var  language = "español";
+
+/*-- Enseñar Menu --*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -12,21 +13,21 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*--Activar Desactivar*/
+/*-- Activar Desactivar --*/
 const navLink = document.querySelectorAll('.nav-link');   
 
 function linkAction(){
-  /*Active link*/
+  /*-- Activar Link --*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
-  /*Quitar Menu de Movil*/
+  /*-- Quitar Menu de Movil --*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*--Scroll Animation*/
+/*-- Scroll Animation --*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -34,36 +35,36 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*--Scroll Inicio--*/
+/*-- Scroll Inicio --*/
 sr.reveal('.home-title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home-img',{delay: 400}); 
 sr.reveal('.home-social-icon',{ interval: 200}); 
 
-/*-Scroll Sobre Mí--*/
+/*-- Scroll Sobre Mí --*/
 sr.reveal('.about-img',{}); 
 sr.reveal('.about-subtitle',{delay: 400}); 
 sr.reveal('.about-text',{delay: 400}); 
 
-/*-Scroll Habilidades--*/
+/*-- Scroll Habilidades --*/
 sr.reveal('.skills-subtitle',{}); 
 sr.reveal('.skills-text',{}); 
 sr.reveal('.skills-data',{interval: 200}); 
 sr.reveal('.skills-img',{delay: 600});
 
-/*-Scroll Proyectos--*/
+/*-- Scroll Proyectos --*/
 sr.reveal('.proyects-img',{interval: 200}); 
 
-/*S-Scroll Contacto--*/
+/*-- Scroll Contacto --*/
 sr.reveal('.contact-input',{interval: 200}); 
 
-/*--Download CV--*/
+/*-- Download CV --*/
 var link = document.createElement('a');
 link.href = url;
 link.download = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
 link.dispatchEvent(new MouseEvent('click'));
 
-/*--Language--*/
+/*-- Language --*/
 function change(){
     if(language == "hola"){
         document.getElementById("español").hidden = true;
