5903f4787b9c3b899a3c3e5479ed8ebcaae818ad
adrianrdguez <adrian.rodriguez@singularfactory.com>
2021-07-26 14:04:04 +0100

    Pop Up Menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 /*------ MENU SHOW Y HIDDEN ------*/
-
+const   navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
 
 /*------ MENU SHOW ------*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.style.bottom = 0;
+        navToggle.style.visibility = 'hidden';
+    })
+}     
 
 /*------ MENU HIDDEN ------*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.style.bottom = '-100%';
+        navToggle.style.visibility = 'visible';
+    })
+} 
 
 /*------ REMOVE MENU MOBILE ------*/
 
