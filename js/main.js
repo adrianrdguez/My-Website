2cd8aa4d83318edb9c1f0d6239e620b22291d2fa
adrianrdguez <adrian.rodriguez@singularfactory.com>
2021-07-27 12:56:54 +0100

    skills without js

--- js/main.js
+++ js/main.js
@@ -33,6 +33,19 @@ navLink.forEach(e => e.addEventListener('click', linkAction))
 
 
 /*------ ACCORDION SKILLS ------*/
+const   skillsContent = document.getElementsByClassName('skills__content'),
+        skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.getElementsByClassName
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
 
 
 /*------ QUALIFICATION TABS ------*/
