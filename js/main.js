cc71ac412a3742f65b4305d47fb6e439f28d773d
adrianrdguez <adrian.rodriguez@singularfactory.com>
2021-07-31 11:50:36 +0200

    Animation Qualification Done

--- js/main.js
+++ js/main.js
@@ -53,6 +53,24 @@ skillsHeader.forEach((e) =>{
 
 
 /*------ QUALIFICATION TABS ------*/
+const   tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})  
 
 
 /*------ SERVICES MODAL ------*/
@@ -64,7 +82,7 @@ skillsHeader.forEach((e) =>{
 /*------ TESTIMONIAL ------*/
 
 
-/*------ SCROLL SECTIONS ACTIVE LINK ------*/
+
 
 
 /*------ CHANGE BACKGROUND HEADER ------*/ 
