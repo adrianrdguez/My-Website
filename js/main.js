9f552c3f054c7a9ffd6827d26302d41b5c12da7d
adrianrdguez <adrian.rodriguez@singularfactory.com>
2021-10-14 09:20:41 +0100

    Dark theme

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ tabs.forEach(tab =>{
 })  
 
 
-/*------ SERVICES MODAL ------*/
+ 
 
 
 /*------ PORTFOLIO SWIPER  ------*/
@@ -91,14 +91,60 @@ let swiper = new Swiper(".portfolio__container", {
   }); 
 
 
-/*------ TESTIMONIAL ------*/
+/*------ SCROLL SECTIONS ACTIVE LINK ------*/
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.scrollY;
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 
 /*------ CHANGE BACKGROUND HEADER ------*/ 
 
+function scrollHeader() {
+    const nav = document.getElementById('header');
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*------ SHOW SCROLL UP ------*/ 
 
 
 /*------ DARK LIGHT THEME ------*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.body.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' :  'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' :  'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', ()=> {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
